5d4726eeb996d4d1b3698b96916e1a06c1ef0a05
wilmer142 <wilmercollege@gmail.com>
2020-01-08 14:45:22 -0500

    Implementing sweet alerts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,7 @@ export class AuthService {
     })
     .catch(error => {
       console.log(error);
+      Swal.fire('Error', error.message, 'error');
     })
   }
 
@@ -26,6 +28,7 @@ export class AuthService {
     })
     .catch(error => {
       console.log(error);
+      Swal.fire('Error', error.message, 'error');
     })
   }
 }
